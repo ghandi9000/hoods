c82223f48060b6e8bef79fd32b89a66f7fd2387b
Noah Peart <noah.v.peart@gmail.com>
2015-04-15 17:01:14 -0400

    Batch Update

--- models/hood_functions.R
+++ models/hood_functions.R
@@ -3,7 +3,7 @@
 ## Description: Neighborhood functions
 ## Author: Noah Peart
 ## Created: Sun Apr 12 18:25:37 2015 (-0400)
-## Last-Updated: Sun Apr 12 21:02:29 2015 (-0400)
+## Last-Updated: Wed Apr 15 17:00:24 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 ## Possible neighborhood sizes: [9, 25, 49]
@@ -27,4 +27,69 @@ findTargets <- function(plot, ndist, yr, dat) {
             samp$BQUDY >= 1+ndist & samp$BQUDY <= 10-ndist
     samp[inds, ]
 }
+
+library(stats4)
+library(MASS)
+library(bbmle)
+set.seed(1001)
+N <- 100
+x <- rnorm(N, mean=3, sd=2)
+LL <- function(mu, sigma) {
+    -sum(dnorm(x, mu, sigma, log=TRUE))
+}
+(distr <- fitdistr(x, "normal"))$loglik
+summary(est1 <- mle(LL, start=list(mu=1, sigma=1)))
+summary(est2 <- mle2(LL, start=list(mu=1, sigma=1)))
+
+## Bounded
+summary(est1 <- mle(LL, start=list(mu=1, sigma=1), method="L-BFGS-B",
+                    lower=c(-Inf, 0), upper=c(Inf, Inf)))
+summary(est2 <- mle2(LL, start=list(mu=1, sigma=1), method="L-BFGS-B",
+                     lower=c(m=-Inf, sigma=0), upper=c(Inf, Inf)))
+
+## Linear model
+x <- runif(N)
+y <- 5*x + 3 + rnorm(N)
+summary(fit <- lm(y ~ x))
+-2*logLik(fit)
+plot(x, y)
+abline(fit, col="red", lty=2)
+
+LL <- function(beta0, beta1, mu, sigma) {
+    R <- y - x*beta1 - beta0
+    R <- suppressWarnings(dnorm(R, mu, sigma))
+    -sum(log(R))
+}
+
+# summary(fit1 <- mle(LL, start=list(beta0=1, beta1=1, mu=0, sigma=1)))
+summary(fit1 <- mle(LL, start=list(beta0=4, beta1=2, mu=0, sigma=1)))  # matches lm
+summary(fit2 <- mle2(LL, start=list(beta0=4, beta1=2, mu=0, sigma=1)))  # matches lm
+
+## Fix mu to 0
+summary(fit3 <- mle(LL, start=list(beta0=4, beta1=2, sigma=1), fixed=list(mu=0)))
+
+## Not inverting Hessian
+(summary(fit4 <- mle2(LL, start=list(beta0=4, beta1=2, mu=0, sigma=1))))
+plot(x, y)
+abline(fit, col="red", lty=2)
+xs <- c(min(x), max(x))
+
+lines(c(min(x), max(x)), c(predict(fit4, newdata = data.frame(x=c(min(x), max(x))))))
+
+
+
+
+## DBH distribution
+hist(na.omit(pp$DBH), freq=FALSE)
+
+## Exponential
+dDBHexp <- fitdistr(na.omit(pp$DBH), densfun="exponential")
+curve(coef(tst2)*exp(-coef(tst2)*x), add=T)
+
+## Weibull
+dDBHweibull <- fitdistr(na.omit(pp[!is.na(pp$DBH)&pp$DBH>0,]$DBH), densfun="weibull")
+k <- coef(dDBHweibull)[["shape"]]
+lambda <- coef(dDBHweibull)[["scale"]]
+curve(k/lambda * (x/k)^(k - 1) * exp(-x/lambda)^k, add=TRUE, from=0, to=60, lty=2, col="green")
+dDBHbinom <- fitdistr(na.omit(pp$DBH), densfun="negative binomial")
 
