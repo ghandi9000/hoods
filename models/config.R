054eabe4797446cbef78d37ef6e4d4e37dde74cf
Noah Peart <noah.v.peart@gmail.com>
2015-04-21 14:16:37 -0400

    ups

--- models/config.R
+++ models/config.R
@@ -4,14 +4,14 @@
 ## Description: 
 ## Author: Noah Peart
 ## Created: Tue Apr 14 12:05:33 2015 (-0400)
-## Last-Updated: Tue Apr 14 12:33:46 2015 (-0400)
+## Last-Updated: Tue Apr 21 12:32:08 2015 (-0400)
 ##           By: Noah Peart
 ######################################################################
 cat("-----------------------------------------------------\n")
 cat("\n\n\t\tChecking/installing packages...\n\n")
 cat("-----------------------------------------------------\n")
 packages <- c("dplyr", "plyr", "xtable", "shiny", "magrittr",
-              "lazyeval", "ggplot2", "grid")  # required packages
+              "lazyeval", "ggplot2", "grid", "bbmle")  # required packages
 for (pack in packages) {
     cat(paste("-->", pack, "\n"))
     if (pack %in% rownames(installed.packages()) == FALSE)
